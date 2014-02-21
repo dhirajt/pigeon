3f36cbe3e2f7d0172efde94a11ac8d8e0eddb8c9
Dhiraj Thakur <dhirajthakur92@facebook.com>
2014-02-21 13:02:05 +0530

    added authentication detection

--- __init__.py
+++ __init__.py
@@ -2,21 +2,35 @@
 
 import requests
 
-BASE_URL = 'http://site2.way2sms.com/'
-LOGIN_URL = 'http://site2.way2sms.com/Login1.action'
+
+class Error(Exception):
+
+    """
+    Base class for exceptions in this module.
+    """
+    pass
+
+
+class AuthenticationError(Error):
+
+    "You must provide a valid username and password."
+
 
 class Way2SMS(object):
+
     """
-    
     API for way2sms (http://www.way2sms.com)
-    
     """
+
+    BASE_URL = 'http://site2.way2sms.com/'
+    LOGIN_URL = 'http://site2.way2sms.com/Login1.action'
+
     def __init__(self, sender=None, password=None):
         super(Way2SMS, self).__init__()
         self.sender = sender
         self.password = password
 
-    def login(self,sender=None,password=None):
+    def login(self, sender=None, password=None):
         self.session = requests.session()
 
         if sender:
@@ -25,11 +39,18 @@ class Way2SMS(object):
             self.password = password
 
         post_data = {
-            'username':self.sender,
-            'password':self.password
+            'username': self.sender,
+            'password': self.password
         }
 
-        response = self.session.post(LOGIN_URL,post_data)
+        response = self.session.post(Way2SMS.LOGIN_URL, post_data)
+        session_id = ''
+
         for cookie in self.session.cookies:
             if cookie.name == 'JSESSIONID':
                 self.auth_cookie = cookie
+                session_id = self.auth_cookie.value.split('~')[1]
+
+        if not session_id in response.url:
+            raise AuthenticationError(
+                "You must provide a valid username and password.")
